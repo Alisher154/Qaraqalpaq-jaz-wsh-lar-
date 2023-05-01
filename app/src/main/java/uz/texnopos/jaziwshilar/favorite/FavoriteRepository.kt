ebb62877f7ae523f67f9dd8d061505acd5b1562c
Batir2003 <dalibaevbatirbay03@gmail.com>
2023-05-02 01:14:55 +0500

    moved to IO thread

--- app/src/main/java/uz/texnopos/jaziwshilar/favorite/FavoriteRepository.kt
+++ app/src/main/java/uz/texnopos/jaziwshilar/favorite/FavoriteRepository.kt
@@ -1,10 +1,16 @@
 package uz.texnopos.jaziwshilar.favorite
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import uz.texnopos.jaziwshilar.data.Poet
 import uz.texnopos.jaziwshilar.data.PoetsDao
 
 class FavoriteRepository(private val dao: PoetsDao) {
 
-    suspend fun getAllFavorites():List<Poet> = dao.getAllFavorites()
+    suspend fun getAllFavorites(): List<Poet> {
+        return withContext(Dispatchers.IO) {
+            dao.getAllFavorites()
+        }
+    }
 
 }
